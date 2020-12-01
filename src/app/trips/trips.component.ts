5e6efa333faaf2ddb4b14adbede3f78037b82495
jankos1195 <jankos1195@gmail.com>
2020-12-02 00:16:47 +0100

    lab 1 done

--- src/app/trips/trips.component.ts
+++ src/app/trips/trips.component.ts
@@ -17,17 +17,44 @@ export class TripsComponent implements OnInit {
   example_trips: TripStructure[] = [
     { name: 'Weekend in Warsaw', destination: 'Warsaw', start_date: '01.01.2021', end_date: '31.01.2021', price: 1800, availableSeats: 5, maxSeats: 5, description: 'Lorem ipsum', img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Panorama_siekierkowski.jpg/1800px-Panorama_siekierkowski.jpg' },
     { name: 'Torun\'s Pastries', destination: 'Torun', start_date: '01.02.2021', end_date: '28.02.2021', price: 750, availableSeats: 10, maxSeats: 10, description: 'Lorem ipsum', img: 'https://bi.im-g.pl/im/33/db/17/z25016371V.jpg' },
-    { name: 'Zakopane for sports', destination: 'Zakopane', start_date: '01.04.2021', end_date: '30.04.2021', price: 1350, availableSeats: 15, maxSeats: 15, description: 'Lorem ipsum', img: 'https://ocdn.eu/pulscms-transforms/1/imJk9kpTURBXy80MTZlNDUyZGU0ZjY3MDRhYzI3OGY3Y2I5YjM2MWY2Zi5qcGeTlQMAzKrNFUHNC_STBc0DFM0BvJMJpjlhN2Y1MwaBoTAB/zakopane-i-tatry-noca.jpg' },
+    { name: 'Zakopane for sports', destination: 'Zakopane', start_date: '01.04.2021', end_date: '30.04.2021', price: 1800, availableSeats: 15, maxSeats: 15, description: 'Lorem ipsum', img: 'https://ocdn.eu/pulscms-transforms/1/imJk9kpTURBXy80MTZlNDUyZGU0ZjY3MDRhYzI3OGY3Y2I5YjM2MWY2Zi5qcGeTlQMAzKrNFUHNC_STBc0DFM0BvJMJpjlhN2Y1MwaBoTAB/zakopane-i-tatry-noca.jpg' },
     { name: 'Truth about Kazimierz', destination: 'Kazimierz The Great', start_date: '01.05.2021', end_date: '31.05.2021', price: 450, availableSeats: 20, maxSeats: 20, description: 'Lorem ipsum', img: 'https://krakow.pl/zalacznik/295939/4.jpg' },
-    { name: 'History of Cracow', destination: 'Cracow', start_date: '01.06.2021', end_date: '30.06.2021', price: 800, availableSeats: 5, maxSeats: 5, description: 'Lorem ipsum', img: 'https://meteor-turystyka.pl/images/places/0/28.jpg' },
+    { name: 'History of Cracow', destination: 'Cracow', start_date: '01.06.2021', end_date: '30.06.2021', price: 350, availableSeats: 5, maxSeats: 5, description: 'Lorem ipsum', img: 'https://meteor-turystyka.pl/images/places/0/28.jpg' },
     { name: 'Sunny Baltic Sea', destination: 'Kolobrzeg', start_date: '01.07.2021', end_date: '31.07.2021', price: 950, availableSeats: 35, maxSeats: 35, description: 'Lorem ipsum', img: 'https://przegladbaltycki.pl/wp-content/uploads/2020/09/Kolobrzeg-molo.jpg' },
-    { name: 'Masuria on sails', destination: 'Elk', start_date: '01.08.2021', end_date: '31.08.2021', price: 300, availableSeats: 10, maxSeats: 10, description: 'Lorem ipsum', img: 'https://pliki.portalsamorzadowy.pl/i/15/27/91/152791_r0_940.jpg' },
+    { name: 'Masuria on sails', destination: 'Elk', start_date: '01.08.2021', end_date: '31.08.2021', price: 300, availableSeats: 10, maxSeats: 10, description: 'Lorem ipsum', img: 'https://pliki.portalsamorzadowy.pl/i/15/27/91/152791_r0_940.jpg' }
   ];
+
+  borderPrices = {
+    low: {value: 0, id: [0]}, 
+    high: {value: 0, id: [0]}
+  };
 
   constructor() { 
     for (let i = 0; i <  this.example_trips.length; i++) {
       this.example_trips[i].description = this.temp_desc;
+      if (i == 0) {
+        this.borderPrices.low.value = this.example_trips[i].price;
+        this.borderPrices.high.value = this.example_trips[i].price;
+        this.borderPrices.low.id = [i];
+        this.borderPrices.high.id = [i];
+      } else {
+        if (this.example_trips[i].price > this.borderPrices.high.value) {
+          this.borderPrices.high.value = this.example_trips[i].price;
+          this.borderPrices.high.id = [i];
+        } else if (this.example_trips[i].price == this.borderPrices.high.value) {
+          this.borderPrices.high.id.push(i);
+        }
+
+        if (this.example_trips[i].price < this.borderPrices.low.value) {
+          this.borderPrices.low.value = this.example_trips[i].price;
+          this.borderPrices.low.id = [i];
+        } else if (this.example_trips[i].price == this.borderPrices.low.value) {
+          this.borderPrices.low.id.push(i);
+        }
+      }
     }
+
+
   }
 
   ngOnInit(): void {
