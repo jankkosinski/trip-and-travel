a9ea63d77c646acad704a1391dc2ca5476699c19
jankos1195 <jankos1195@gmail.com>
2020-12-16 04:17:10 +0100

    new-trip - mockup

--- src/app/components/new-trip/new-trip.component.ts
+++ src/app/components/new-trip/new-trip.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewTripComponent implements OnInit {
 
+  panelOpenState: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
