0b52209bc4986d15875e1a9e3eed15b9cbb005b3
jankos1195 <jankos1195@gmail.com>
2020-12-18 00:08:36 +0100

    firebase init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCLdJA4J08uEuGZ5sempV38K9TboAeedoI",
+    authDomain: "tripandtravel-serverless.firebaseapp.com",
+    projectId: "tripandtravel-serverless",
+    storageBucket: "tripandtravel-serverless.appspot.com",
+    messagingSenderId: "486152451215",
+    appId: "1:486152451215:web:1a85ede4ef25a3deceacfa"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCLdJA4J08uEuGZ5sempV38K9TboAeedoI",
+    authDomain: "tripandtravel-serverless.firebaseapp.com",
+    projectId: "tripandtravel-serverless",
+    storageBucket: "tripandtravel-serverless.appspot.com",
+    messagingSenderId: "486152451215",
+    appId: "1:486152451215:web:1a85ede4ef25a3deceacfa"
+  }
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
